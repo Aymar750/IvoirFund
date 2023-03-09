1d993d2e25ea25990a25b92e0de9f5beb528c7cd
Aymar <yannyao4066@gmail.com>
2023-03-09 23:03:46 +0000

    frontend page accueil [OK]

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './Components/home/home.component';
+import { MainComponent } from './Components/main/main.component';
+import { CategorieComponent } from './Components/categorie/categorie.component';
+import { ProjetComponent } from './Components/projet/projet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    MainComponent,
+    CategorieComponent,
+    ProjetComponent
   ],
   imports: [
     BrowserModule,
