482deb9362ce33c4c0937dc9396636fa115c7740
Aymar <yannyao4066@gmail.com>
2023-03-16 18:01:18 +0000

    recupUser [OK]

--- frontend/src/app/Components/dashcompte/dashcompte.component.ts
+++ frontend/src/app/Components/dashcompte/dashcompte.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-dashcompte',
   templateUrl: './dashcompte.component.html',
   styleUrls: ['./dashcompte.component.css']
 })
-export class DashcompteComponent {
+
+export class DashcompteComponent implements OnInit {
+  nom: string | null = null;
+ 
+  constructor(private authService : AuthService) { }
+  ngOnInit(): void {
+    const userString = localStorage.getItem('user');
+    if(userString){
+      const user = JSON.parse(userString);
+      this.authService.getUserProfile(user.id).subscribe({
+        next: (res) => {
+            this.nom = res.name;
+        }
+      })
+    }
+  }
   
 }
